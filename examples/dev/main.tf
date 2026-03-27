8282e3946e1332e435de829939c464af3ddca683
Wei Wang <weiww@google.com>
2026-03-27 00:50:58 +0000

    modify parameters for dev test

--- examples/dev/main.tf
+++ examples/dev/main.tf
@@ -1,15 +1,15 @@
 module "cluster_automation" {
-  source                       = "git::https://github.com/GDC-ConsumerEdge/automated-cluster-provisioner.git?ref=v1.3.2"
-  project_id                   = "sample-project-id"
-  source_of_truth_repo         = "github.com/GDC-ConsumerEdge/automated-cluster-provisioner"
-  git_secret_id                = "example-pat-token"
-  project_id_secrets           = "example-secrets-project"
+  source                       = "../../module"
+  project_id                   = "cloud-alchemists-sandbox"
+  source_of_truth_repo         = "github.com/weiwang118/automated-cluster-provisioner"
+  git_secret_id                = "wei-acp-test"
+  project_id_secrets           = "cloud-alchemists-sandbox"
   source_of_truth_path         = "example-source-of-truth.csv"
   region                       = "us-central1"
   environment                  = "dev"
   cluster_creation_timeout     = "86400"
   cluster_creation_max_retries = "0"
-  notification_channel_email   = "ops@example.com"
+  notification_channel_email   = "weiww@google.com"
   opt_in_build_messages        = true
   default_config_sync_version  = "1.18.3"
 }
