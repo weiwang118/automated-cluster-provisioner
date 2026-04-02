1ca223c1467cf0a10073678d92c849d1e38f7845
Wei Wang <weiww@google.com>
2026-04-02 23:06:30 +0000

    update source of truth

--- examples/dev/main.tf
+++ examples/dev/main.tf
@@ -1,10 +1,10 @@
 module "cluster_automation" {
-  source                       = "../../module"
+  source                       = "git::https://github.com/weiwang118/automated-cluster-provisioner.git?ref=sandbox-test"
   project_id                   = "cloud-alchemists-sandbox"
   source_of_truth_repo         = "github.com/weiwang118/automated-cluster-provisioner"
   git_secret_id                = "wei-acp-test"
   project_id_secrets           = "cloud-alchemists-sandbox"
-  source_of_truth_path         = "example-source-of-truth.csv"
+  source_of_truth_path         = "wei-source-of-truth.csv"
   region                       = "us-central1"
   environment                  = "wei-dev"
   cluster_creation_timeout     = "86400"
