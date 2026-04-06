54339c9724251dc9a990b1df61a749da93cb8ca6
Wei Wang <weiww@google.com>
2026-04-06 22:44:14 +0000

    enable robin cns in a cluster level for create cluster action

--- module/watchers/src/cluster_intent_model.py
+++ module/watchers/src/cluster_intent_model.py
@@ -42,6 +42,7 @@ class SourceOfTruthModel(BaseModel):
     labels: Optional[str] = None
     backup_enable: Optional[bool] = None
     recreate_on_delete: Optional[bool] = None
+    enable_robin_cns: Optional[bool] = None
 
     @validator('*', pre=True)
     def convert_to_none(cls, v):
